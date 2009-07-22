74bbfaff991458183b233426b5edecbcd4b97db8
PanteR <panter.dsd@gmail.com>
2009-07-22 16:49:38 +0400

    Cosmetic fix.

--- src/Preferences/qtoolbuttonpreference.cpp
+++ src/Preferences/qtoolbuttonpreference.cpp
@@ -27,109 +27,101 @@ QToolButtonPreference::QToolButtonPreference(QWidget* parent)
 //
 void QToolButtonPreference::createControls()
 {
-	qlCommand=new QLabel(tr("Command"),this);
-	qleCommand=new QLineEdit(this);
-	qtbCommand=new QToolButton(this);
-	qtbCommand->setText(">>");
+	qlCommand = new QLabel(tr("Command"), this);
+	qleCommand = new QLineEdit(this);
+	qtbCommand = new QToolButton(this);
+	qtbCommand->setText(QLatin1String(">>"));
 	qtbCommand->setToolButtonStyle(Qt::ToolButtonTextOnly);
 
-	qlParams=new QLabel(tr("Parameters"),this);
-	qleParams=new QLineEdit(this);
+	qlParams = new QLabel(tr("Parameters"), this);
+	qleParams = new QLineEdit(this);
 
-	qlWorkDir=new QLabel(tr("Work dir"),this);
-	qleWorkDir=new QLineEdit(this);
-	qtbWorkDir=new QToolButton(this);
-	qtbWorkDir->setText(">>");
+	qlWorkDir = new QLabel(tr("Work dir"), this);
+	qleWorkDir = new QLineEdit(this);
+	qtbWorkDir = new QToolButton(this);
+	qtbWorkDir->setText(QLatin1String(">>"));
 	qtbWorkDir->setToolButtonStyle(Qt::ToolButtonTextOnly);
 
-	qlIconFile=new QLabel(tr("Icon file"),this);
-	qleIconFile=new QLineEdit(this);
-	qtbIconFile=new QToolButton(this);
-	qtbIconFile->setText(">>");
+	qlIconFile = new QLabel(tr("Icon file"), this);
+	qleIconFile = new QLineEdit(this);
+	qtbIconFile = new QToolButton(this);
+	qtbIconFile->setText(QLatin1String(">>"));
 	qtbIconFile->setToolButtonStyle(Qt::ToolButtonTextOnly);
 
-	qlIcon=new QLabel(tr("Icon"),this);
-	qlwIcons=new QListWidget(this);
+	qlIcon = new QLabel(tr("Icon"), this);
+	qlwIcons = new QListWidget(this);
 	qlwIcons->setFlow(QListView::LeftToRight);
 	qlwIcons->setViewMode(QListView::IconMode);
-	qlwIcons->setIconSize(QSize(32,32));
+	qlwIcons->setIconSize(QSize(32, 32));
 	qlwIcons->setWrapping(false);
-	qlwIcons->setSizePolicy(QSizePolicy::Preferred,QSizePolicy::Maximum);
+	qlwIcons->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Maximum);
 
-	qlCaption=new QLabel(tr("Caption"),this);
-	qleCaption=new QLineEdit(this);
+	qlCaption = new QLabel(tr("Caption"), this);
+	qleCaption = new QLineEdit(this);
 }
 //
 void QToolButtonPreference::setLayouts()
 {
-	QGridLayout* qglMainLayout=new QGridLayout();
+	QGridLayout* qglMainLayout = new QGridLayout();
 
-	QHBoxLayout* qhblCommandLayout=new QHBoxLayout();
+	QHBoxLayout* qhblCommandLayout = new QHBoxLayout();
 	qhblCommandLayout->addWidget(qleCommand);
 	qhblCommandLayout->addWidget(qtbCommand);
 
-	QHBoxLayout* qhblWorkDirLayout=new QHBoxLayout();
+	QHBoxLayout* qhblWorkDirLayout = new QHBoxLayout();
 	qhblWorkDirLayout->addWidget(qleWorkDir);
 	qhblWorkDirLayout->addWidget(qtbWorkDir);
 
-	QHBoxLayout* qhblIconFileLayout=new QHBoxLayout();
+	QHBoxLayout* qhblIconFileLayout = new QHBoxLayout();
 	qhblIconFileLayout->addWidget(qleIconFile);
 	qhblIconFileLayout->addWidget(qtbIconFile);
 
-	qglMainLayout->addWidget(qlCommand,0,0);
-	qglMainLayout->addLayout(qhblCommandLayout,0,1);
-	qglMainLayout->addWidget(qlParams,1,0);
-	qglMainLayout->addWidget(qleParams,1,1);
-	qglMainLayout->addWidget(qlWorkDir,2,0);
-	qglMainLayout->addLayout(qhblWorkDirLayout,2,1);
-	qglMainLayout->addWidget(qlIconFile,3,0);
-	qglMainLayout->addLayout(qhblIconFileLayout,3,1);
-	qglMainLayout->addWidget(qlIcon,4,0);
-	qglMainLayout->addWidget(qlwIcons,4,1);
-	qglMainLayout->addWidget(qlCaption,5,0);
-	qglMainLayout->addWidget(qleCaption,5,1);
+	qglMainLayout->addWidget(qlCommand, 0, 0);
+	qglMainLayout->addLayout(qhblCommandLayout, 0, 1);
+	qglMainLayout->addWidget(qlParams, 1, 0);
+	qglMainLayout->addWidget(qleParams, 1, 1);
+	qglMainLayout->addWidget(qlWorkDir, 2, 0);
+	qglMainLayout->addLayout(qhblWorkDirLayout, 2, 1);
+	qglMainLayout->addWidget(qlIconFile, 3, 0);
+	qglMainLayout->addLayout(qhblIconFileLayout, 3, 1);
+	qglMainLayout->addWidget(qlIcon, 4, 0);
+	qglMainLayout->addWidget(qlwIcons, 4, 1);
+	qglMainLayout->addWidget(qlCaption, 5, 0);
+	qglMainLayout->addWidget(qleCaption, 5, 1);
 
 	this->setLayout(qglMainLayout);
 }
 //
 void QToolButtonPreference::setConnects()
 {
-	connect(qtbCommand,
-			SIGNAL(clicked()),
-			this,
-			SLOT(slotChooseCommandFile()));
-	connect(qtbWorkDir,
-			SIGNAL(clicked()),
-			this,
-			SLOT(slotChooseWorkDir()));
-	connect(qtbIconFile,
-			SIGNAL(clicked()),
-			this,
-			SLOT(slotChooseIconFile()));
-	connect(qleIconFile,
-			SIGNAL(textChanged(QString)),
-			this,
-			SLOT(slotGetIconList(QString)));
+	connect(qtbCommand, SIGNAL(clicked()),
+			this, SLOT(slotChooseCommandFile()));
+	connect(qtbWorkDir, SIGNAL(clicked()),
+			this, SLOT(slotChooseWorkDir()));
+	connect(qtbIconFile, SIGNAL(clicked()),
+			this, SLOT(slotChooseIconFile()));
+	connect(qleIconFile, SIGNAL(textChanged(QString)),
+			this, SLOT(slotGetIconList(QString)));
 }
 //
 void QToolButtonPreference::slotChooseCommandFile()
 {
 	QString command;
-	command=QFileDialog::getOpenFileName(this,
+	command = QFileDialog::getOpenFileName(this,
 										tr("Choose command file"),
 										QDir(qleCommand->text()).absolutePath());
-	if (command.isEmpty())
-		return;
-	qleCommand->setText(QDir::toNativeSeparators(command));
-	qleWorkDir->setText(QDir::toNativeSeparators(QFileInfo(command).absolutePath()));
-	qleIconFile->setText(QDir::toNativeSeparators(command));
-	qleCaption->setText(QFileInfo(command).baseName());
+	if (!command.isEmpty()) {
+		qleCommand->setText(QDir::toNativeSeparators(command));
+		qleWorkDir->setText(QDir::toNativeSeparators(QFileInfo(command).absolutePath()));
+		qleIconFile->setText(QDir::toNativeSeparators(command));
+		qleCaption->setText(QFileInfo(command).baseName());
+	}
 }
 //
 void QToolButtonPreference::slotChooseWorkDir()
 {
 	QString dir;
-	dir=QFileDialog::getExistingDirectory(this,
+	dir = QFileDialog::getExistingDirectory(this,
 										tr("Choose work dir"),
 										QDir(qleWorkDir->text()).absolutePath());
 	if (!dir.isEmpty())
@@ -140,12 +132,11 @@ void QToolButtonPreference::slotChooseIconFile()
 {
 	QString iconFileName;
 	QStringList qslFilter;
-	iconFileName=QFileDialog::getOpenFileName(this,
+	iconFileName = QFileDialog::getOpenFileName(this,
 										tr("Choose command file"),
 										QDir(qleIconFile->text()).absolutePath());
-	if (iconFileName.isEmpty())
-		return;
-	qleIconFile->setText(QDir::toNativeSeparators(iconFileName));
+	if (!iconFileName.isEmpty())
+		qleIconFile->setText(QDir::toNativeSeparators(iconFileName));
 }
 //
 void QToolButtonPreference::slotGetIconList(const QString& iconFileName)
@@ -154,27 +145,24 @@ void QToolButtonPreference::slotGetIconList(const QString& iconFileName)
 	qlwIcons->clear();
 	HICON smallIcon,lageIcon;
 	UINT iconCount = ExtractIconExW((wchar_t*)iconFileName.utf16(), -1, 0, 0, 0);
-	qlwIcons->clear();
 	QListWidgetItem* item;
-	for (int i=0; i<int(iconCount); i++)
-	{
-		if (ExtractIconExW((wchar_t*)iconFileName.utf16(), i, &lageIcon, &smallIcon, 1))
-		{
+	for (int i = 0; i < int(iconCount); i++) {
+		if (ExtractIconExW((wchar_t*)iconFileName.utf16(), i, &lageIcon, &smallIcon, 1)) {
 			ICONINFO info;
 			QPixmap pixmap;
 			QIcon icon;
 			//Lage
-			GetIconInfo(lageIcon,&info);
-			pixmap=QPixmap::fromWinHBITMAP(info.hbmColor,QPixmap::NoAlpha);
-			pixmap.setMask(QPixmap::fromWinHBITMAP(info.hbmMask,QPixmap::NoAlpha));
+			GetIconInfo(lageIcon, &info);
+			pixmap = QPixmap::fromWinHBITMAP(info.hbmColor, QPixmap::NoAlpha);
+			pixmap.setMask(QPixmap::fromWinHBITMAP(info.hbmMask, QPixmap::NoAlpha));
 			icon.addPixmap(pixmap);
 			//Small
-			GetIconInfo(smallIcon,&info);
-			pixmap=QPixmap::fromWinHBITMAP(info.hbmColor,QPixmap::NoAlpha);
-			pixmap.setMask(QPixmap::fromWinHBITMAP(info.hbmMask,QPixmap::NoAlpha));
+			GetIconInfo(smallIcon, &info);
+			pixmap = QPixmap::fromWinHBITMAP(info.hbmColor, QPixmap::NoAlpha);
+			pixmap.setMask(QPixmap::fromWinHBITMAP(info.hbmMask, QPixmap::NoAlpha));
 			icon.addPixmap(pixmap);
 			//
-			item=new QListWidgetItem(icon,QString::number(i),qlwIcons);
+			item = new QListWidgetItem(icon, QString::number(i), qlwIcons);
 			qlwIcons->addItem(item);
 		}
 	}
@@ -188,7 +176,7 @@ void QToolButtonPreference::slotGetIconList(const QString& iconFileName)
 //
 void QToolButtonPreference::setButton(const SToolBarButton& button)
 {
-	stbbButton=button;
+	stbbButton = button;
 	qleCommand->setText(button.qsCommand);
 	qleParams->setText(button.qsParams);
 	qleWorkDir->setText(button.qsWorkDir);
@@ -199,15 +187,14 @@ void QToolButtonPreference::setButton(const SToolBarButton& button)
 //
 SToolBarButton QToolButtonPreference::getButton()
 {
-	stbbButton.qsCommand=qleCommand->text();
-	stbbButton.qsParams=qleParams->text();
-	stbbButton.qsWorkDir=qleWorkDir->text();
-	stbbButton.qsCaption=qleCaption->text();
-	stbbButton.qsIconFile=qleIconFile->text();
-	if (qlwIcons->count()>0)
-	{
-		stbbButton.qiIcon=qlwIcons->currentItem()->icon();
-		stbbButton.iconNumber=qlwIcons->currentIndex().row();
+	stbbButton.qsCommand = qleCommand->text();
+	stbbButton.qsParams = qleParams->text();
+	stbbButton.qsWorkDir = qleWorkDir->text();
+	stbbButton.qsCaption = qleCaption->text();
+	stbbButton.qsIconFile = qleIconFile->text();
+	if (qlwIcons->count() > 0) {
+		stbbButton.qiIcon = qlwIcons->currentItem()->icon();
+		stbbButton.iconNumber = qlwIcons->currentIndex().row();
 	}
 	return stbbButton;
 }
@@ -215,32 +202,31 @@ SToolBarButton QToolButtonPreference::getButton()
 SToolBarButton QToolButtonPreference::getButton(const QString& command)
 {
 	SToolBarButton button;
-	button.qsCommand=QDir::toNativeSeparators(command);
-	button.qsWorkDir=QDir::toNativeSeparators(QFileInfo(command).absolutePath());
-	button.qsIconFile=button.qsCommand;
-	button.qsCaption=QFileInfo(command).baseName();
-	button.iconNumber=-1;
+	button.qsCommand = QDir::toNativeSeparators(command);
+	button.qsWorkDir = QDir::toNativeSeparators(QFileInfo(command).absolutePath());
+	button.qsIconFile = button.qsCommand;
+	button.qsCaption = QFileInfo(command).baseName();
+	button.iconNumber = -1;
 #ifdef Q_WS_WIN
-	HICON smallIcon,lageIcon;
+	HICON smallIcon, lageIcon;
 	UINT iconCount = ExtractIconExW((wchar_t*)button.qsIconFile.utf16(),-1,0,0,0);
-	if ((iconCount > 0) && (ExtractIconExW((wchar_t*)button.qsIconFile.utf16(), 0, &lageIcon, &smallIcon, 1) > 0))
-	{
+	if ((iconCount > 0) && (ExtractIconExW((wchar_t*)button.qsIconFile.utf16(), 0, &lageIcon, &smallIcon, 1) > 0)) {
 		ICONINFO info;
 		QPixmap pixmap;
 		QIcon icon;
 		//Lage
-		GetIconInfo(lageIcon,&info);
-		pixmap=QPixmap::fromWinHBITMAP(info.hbmColor,QPixmap::NoAlpha);
-		pixmap.setMask(QPixmap::fromWinHBITMAP(info.hbmMask,QPixmap::NoAlpha));
+		GetIconInfo(lageIcon, &info);
+		pixmap = QPixmap::fromWinHBITMAP(info.hbmColor, QPixmap::NoAlpha);
+		pixmap.setMask(QPixmap::fromWinHBITMAP(info.hbmMask, QPixmap::NoAlpha));
 		icon.addPixmap(pixmap);
 		//Small
-		GetIconInfo(smallIcon,&info);
-		pixmap=QPixmap::fromWinHBITMAP(info.hbmColor,QPixmap::NoAlpha);
-		pixmap.setMask(QPixmap::fromWinHBITMAP(info.hbmMask,QPixmap::NoAlpha));
+		GetIconInfo(smallIcon, &info);
+		pixmap = QPixmap::fromWinHBITMAP(info.hbmColor, QPixmap::NoAlpha);
+		pixmap.setMask(QPixmap::fromWinHBITMAP(info.hbmMask, QPixmap::NoAlpha));
 		icon.addPixmap(pixmap);
 		//
-		button.iconNumber=0;
-		button.qiIcon=icon;
+		button.iconNumber = 0;
+		button.qiIcon = icon;
 	} else {
 		button.iconNumber = 0;
 		button.qiIcon = QFileIconProvider().icon(QFileInfo(button.qsIconFile));
--- src/Preferences/qtoolbuttonpreference.h
+++ src/Preferences/qtoolbuttonpreference.h
@@ -35,50 +35,52 @@ class QAction;
 //
 struct SToolBarButton
 {
-	QString		qsCommand;
-	QString		qsParams;
-	QString		qsWorkDir;
-	QString		qsIconFile;
-	int				iconNumber;
-	QIcon			qiIcon;
-	QString		qsCaption;
+	QString qsCommand;
+	QString qsParams;
+	QString qsWorkDir;
+	QString qsIconFile;
+	int iconNumber;
+	QIcon qiIcon;
+	QString qsCaption;
 };
 class QToolButtonPreference : public QWidget
 {
-Q_OBJECT
-public:
+	Q_OBJECT
 private:
-	QLabel*								qlCommand;
-	QLineEdit*							qleCommand;
-	QToolButton*						qtbCommand;
+	QLabel *qlCommand;
+	QLineEdit *qleCommand;
+	QToolButton *qtbCommand;
 
-	QLabel*								qlParams;
-	QLineEdit*							qleParams;
+	QLabel *qlParams;
+	QLineEdit *qleParams;
 
-	QLabel*								qlWorkDir;
-	QLineEdit*							qleWorkDir;
-	QToolButton*						qtbWorkDir;
+	QLabel *qlWorkDir;
+	QLineEdit *qleWorkDir;
+	QToolButton *qtbWorkDir;
 
-	QLabel*								qlIconFile;
-	QLineEdit*							qleIconFile;
-	QToolButton*						qtbIconFile;
+	QLabel *qlIconFile;
+	QLineEdit *qleIconFile;
+	QToolButton *qtbIconFile;
 
-	QLabel*								qlIcon;
-	QListWidget*						qlwIcons;
+	QLabel *qlIcon;
+	QListWidget *qlwIcons;
 
-	QLabel*								qlCaption;
-	QLineEdit*							qleCaption;
+	QLabel *qlCaption;
+	QLineEdit *qleCaption;
 
-	SToolBarButton					stbbButton;
+	SToolBarButton stbbButton;
+
 public:
 	QToolButtonPreference(QWidget* parent=0);
 	void setButton(const SToolBarButton& button);
 	SToolBarButton getButton();
 	static SToolBarButton getButton(const QString& command);
+
 private:
 	void createControls();
 	void setLayouts();
 	void setConnects();
+
 private Q_SLOTS:
 	void slotChooseCommandFile();
 	void slotChooseWorkDir();
